ecb38c6647585a2f12c37c421631f6ffaed58873
fratyaldiz <firatylz2004@hotmail.com>
2025-12-21 20:10:19 +0300

    init comit

--- src/Tree1/BinaryTree.java
+++ src/Tree1/BinaryTree.java
@@ -10,7 +10,7 @@ public class BinaryTree {
 
     Node newNode(int data) {
         root = new Node(data);
-        System.out.println(data + " added to the tree" );
+        //System.out.println(data + " added to the tree" );
         return root;
     }
 
@@ -26,6 +26,30 @@ public class BinaryTree {
         else
             root = newNode(data);
         return root;
+    }
+
+    void preOrder(Node root ) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    void inOrder(Node root ) {
+        if (root != null) {
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+    }
+
+    void postOrder(Node root ) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
 
 }
--- src/Tree1/Main.java
+++ src/Tree1/Main.java
@@ -4,17 +4,32 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
         bt.root = bt.insert(bt.root, 10);
-        bt.root = bt.insert(bt.root, 15);
         bt.root = bt.insert(bt.root, 8);
-        bt.root = bt.insert(bt.root, 20);
-        bt.root = bt.insert(bt.root, 4);
+        bt.root = bt.insert(bt.root, 15);
+        bt.root = bt.insert(bt.root, 5);
         bt.root = bt.insert(bt.root, 12);
+        bt.root = bt.insert(bt.root, 20);
+        bt.root = bt.insert(bt.root, 9);
+
+        System.out.print("preOrder: ");
+        bt.preOrder(bt.root);       //10 8 5 9 15 12 20
 
         System.out.println();
+
+        System.out.print("inOrder: ");
+        bt.inOrder(bt.root);        //5 8 9 10 12 15 20
+
+        System.out.println();
+
+        System.out.print("postOrder: ");
+        bt.postOrder(bt.root);      // 5 9 8 12 20 15 10
+
+
+        /*System.out.println();
         System.out.println("Kökün datası: " + bt.root.data);
         System.out.println("Kökün sağındaki datası: " + bt.root.right.data);
         System.out.println("Kökün solundaki datası: " + bt.root.left.data);
         System.out.println("Kökün solunun solundaki data: " + bt.root.left.left.data);
-
+        */
     }
 }
